2127a801ebe9ab8bff4d08551f399cd4d7397382
Tyler Liu <tyler4long@gmail.com>
2024-11-11 12:21:26 -0800

    Finish testing

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Untitled App
+# RingCentral Get Device Info Demo
+
+```
+yarn install
+```
+
+Create a `.env` file and specify credentials. For the list of credentials, please refer to [./src/demo.ts](./src/demo.ts)
+
+```
+yarn demo
+```
--- src/demo.ts
+++ src/demo.ts
@@ -10,7 +10,20 @@ const main = async () => {
   await rc.authorize({
     jwt: process.env.RINGCENTRAL_JWT_TOKEN!,
   });
-  console.log(rc.token?.access_token);
+
+  // list all extension devices
+  const r0 = await rc.restapi().account().extension().device().get();
+  console.log(JSON.stringify(r0, null, 2));
+
+  // fetch the credentials of a device
+  const r = await rc
+    .restapi()
+    .account()
+    .device(process.env.RINGCENTRAL_DEVICE_ID)
+    .sipInfo()
+    .get();
+  console.log(JSON.stringify(r, null, 2));
+
   await rc.revoke();
 };
 main();
